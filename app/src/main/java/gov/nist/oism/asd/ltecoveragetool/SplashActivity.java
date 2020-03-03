04df4711ef3e723df045c1cffc2e1a898f7dd201
cbonoz <chrisdistrict@gmail.com>
2020-03-02 22:23:09 -0500

    splash

---
+++ app/src/main/java/gov/nist/oism/asd/ltecoveragetool/SplashActivity.java
@@ -0,0 +1,52 @@
+package gov.nist.oism.asd.ltecoveragetool;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+//extends AwesomeSplash!
+public class SplashActivity extends AwesomeSplash {
+
+    //DO NOT OVERRIDE onCreate()!
+    //if you need to start some services do it in initSplash()!
+
+    private static final int SPLASH_DURATION = 750;
+
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorActivityBackground); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.ic_lte_tower_175); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(SPLASH_DURATION); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+        //Customize Title
+        configSplash.setTitleSplash(getString(R.string.app_name));
+        configSplash.setTitleTextColor(R.color.white);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(SPLASH_DURATION);
+        configSplash.setAnimTitleTechnique(Techniques.Bounce);
+//        configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
+
+    }
+
+    @Override
+    public void animationsFinished() {
+        Intent intent = new Intent(this, DisclaimerActivity.class);
+        startActivity(intent);
+    }
+}
