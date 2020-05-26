448ef2d957c3912516f087549dde8db176268914
Chris Buonocore <chrisdistrict@gmail.com>
2020-05-26 07:46:10 -0400

    fix indexing (#2)

---
+++ app/src/main/java/com/lte/mapmylte/mapper/www/util/SignalGrade.java
@@ -0,0 +1,19 @@
+package com.lte.mapmylte.mapper.www.util;
+
+public enum SignalGrade {
+    TOP("top"),
+    MIDDLE_LOW("middlelow"),
+    MIDDLE("middle"),
+    LOW("low"),
+    UNKNOWN("");
+
+    private final String value;
+
+    SignalGrade(String top) {
+        value = top;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
